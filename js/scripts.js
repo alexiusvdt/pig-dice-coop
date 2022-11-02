d53d95cdb49539138318fb16006a0ae44d4b047c
Alex Johnson <alex.johnson293@gmail.com>
2022-11-02 14:00:21 -0700

    pass addl tests & display results to user
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,23 @@
 //this only takes 1 input. to add 2 players we'd need two specific constructors or a way to increment where it searches for
 // player name
 function Player() {
-  this.playerScore = {}
+  this.roundScore = 0;
+  this.totalScore = 0;
   this.currentId = 0;
   // this.playerName = document.querySelector("form#player-name"); //update when HTML written
 }
 
-Player.prototype.playerAddScore = function(score) {
-  score.id = this.assignId();
-  this.playerScore = score;
+Player.prototype.playerAddRoundScore = function(score) {
+  // score.id = this.assignId();
+  this.roundScore += score;
+}
+
+Player.prototype.playerResetRoundScore = function() {
+  this.roundScore = 0;
+}
+
+Player.prototype.playerAddTotalScore = function () {
+  this.totalScore += this.roundScore;
 }
 
 Player.prototype.assignId = function() {
@@ -18,19 +27,27 @@ Player.prototype.assignId = function() {
   return this.currentId;
 }
 
-function diceRoller(roundScore) {
+function diceRoller() {
   let rollResult = Math.floor(Math.random() * 6)+1;
   if (rollResult !== 1) {
-    roundScore += rollResult
+    player1.playerAddRoundScore(rollResult);
+    displayResults(rollResult);
   } else {
-    roundScore = 0
+    player1.playerResetRoundScore();
+    displayRolled1(rollResult);
   }
-  return roundScore
+}
+
+function addToTotalScore() {
+  player1.playerAddTotalScore();
+  player1.playerResetRoundScore();
+  displayResults();
 }
 
 
-
 // ui logic
+
+let player1 = new Player();
 
 //this is what sets up the board
 function newGame() {
@@ -40,13 +57,25 @@ function newGame() {
   let roundScore = 0;
 }
 
+function displayResults(currentRoll) {
+  document.getElementById('current-roll').innerText = currentRoll;
+  document.getElementById('round-score').innerText = player1.roundScore;
+  document.getElementById('total-score').innerText = player1.totalScore;
+}
+
+function displayRolled1(currentRoll) {
+  document.getElementById('current-roll').innerText = currentRoll;
+  document.getElementById('round-score').innerText = "You rolled 1, next player's turn!"
+} 
+
 //this selects the appropriate player
-function turn() {
+function turn() {}
   
-  player1.playerAddScore(roundScore);
+// player1.playerAddScore(roundScore);
 
 
-window.addEventListener("load", function (){
-  document.querySelector("form#dice-roller").addEventListener("submit", diceRoller);
-  document.querySelector("form#new-game").addEventListener("submit", newGame);
+window.addEventListener("load", function() {
+  document.getElementById('new-game').addEventListener('click', newGame);
+  document.getElementById('button-roll').addEventListener('click', diceRoller);
+  document.getElementById('button-pass').addEventListener('click', addToTotalScore);
 });
