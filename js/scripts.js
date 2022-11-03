2b2180f6fce6af5d6f046379f2d6c16011119e4e
Alex Johnson <alex.johnson293@gmail.com>
2022-11-03 14:42:03 -0700

    add dice icons for user rolls
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -40,7 +40,72 @@ function currentPlayer() {
 }
 
 function diceRoller() {
+  resetDice();
   let rollResult = Math.floor(Math.random() * 6)+1;
+  evaluateDiceRoll(rollResult);
+}
+
+
+
+
+// UI Logic
+
+// function cheat() {
+//   player2.totalScore = 101;
+// }
+
+let player1 = new Player();
+let player2 = new Player();
+
+//pushing scores & ending turns
+function addToTotalScore() {
+  player1.playerAddTotalScore();
+  player1.playerResetRoundScore();
+  player2.playerAddTotalScore();
+  player2.playerResetRoundScore();
+  displayResultsP1();
+  displayResultsP2();
+  if (player1.totalScore >= 100) {
+    winner = player1;
+    winGame(winner);
+  } else if (player2.totalScore >= 100) {
+    winner = player2;
+    winGame(winner);
+  } else {
+  player1.playerChangeActive();
+  }
+}
+
+function resetDice() {
+  document.querySelector("span#dice1").setAttribute("name", "-hidden");
+  document.querySelector("span#dice2").setAttribute("name", "-hidden");
+  document.querySelector("span#dice3").setAttribute("name", "-hidden");
+  document.querySelector("span#dice4").setAttribute("name", "-hidden");
+  document.querySelector("span#dice5").setAttribute("name", "-hidden");
+  document.querySelector("span#dice6").setAttribute("name", "-hidden");
+}
+
+function evaluateDiceRoll(rollResult) {
+  switch (rollResult) {
+    case (1):
+      document.querySelector("span#dice1").removeAttribute("name");
+      break;
+    case (2):
+      document.querySelector("span#dice2").removeAttribute("name");
+      break;
+    case (3):
+      document.querySelector("span#dice3").removeAttribute("name");
+      break;
+    case (4):
+      document.querySelector("span#dice4").removeAttribute("name");
+      break;
+    case (5):
+      document.querySelector("span#dice5").removeAttribute("name");
+      break;
+    case (6):
+      document.querySelector("span#dice6").removeAttribute("name");
+      break;
+  }
   if (player1.active === 1) {
     if (rollResult !== 1) {
       player1.playerAddRoundScore(rollResult);
@@ -62,36 +127,6 @@ function diceRoller() {
   }
 }
 
-//pushing scores & ending turns
-function addToTotalScore() {
-  player1.playerAddTotalScore();
-  player1.playerResetRoundScore();
-  player2.playerAddTotalScore();
-  player2.playerResetRoundScore();
-  displayResultsP1();
-  displayResultsP2();
-  if (player1.totalScore >= 100) {
-    winner = player1;
-    winGame(winner);
-  } else if (player2.totalScore >= 100) {
-    winner = player2;
-    winGame(winner);
-  } else {
-  player1.playerChangeActive();
-  }
-}
-
-// function cheat() {
-//   player2.totalScore = 101;
-// }
-
-
-// UI Logic
-
-let player1 = new Player();
-let player2 = new Player();
-
-//this is what sets up the board
 function newGame() {
   event.preventDefault()
   Object.keys(player1).forEach(function(key) {
@@ -121,24 +156,24 @@ document.getElementById('winner').removeAttribute("class")
 
 
 function displayResultsP1(currentRoll) {
-  document.getElementById('current-roll').innerText = currentRoll;
+  // document.getElementById('current-roll').innerText = currentRoll;
   document.getElementById('round-score').innerText = player1.roundScore;
   document.getElementById('total-score').innerText = player1.totalScore;
 }
 
 function displayResultsP2(currentRoll) {
-  document.getElementById('current-roll2').innerText = currentRoll;
+  // document.getElementById('current-roll2').innerText = currentRoll;
   document.getElementById('round-score2').innerText = player2.roundScore;
   document.getElementById('total-score2').innerText = player2.totalScore;
 }
 
 function displayRolled1P1(currentRoll) {
-  document.getElementById('current-roll').innerText = currentRoll;
+  // document.getElementById('current-roll').innerText = currentRoll;
   document.getElementById('round-score').innerText = "You rolled 1, next player's turn!"
 } 
 
 function displayRolled1P2(currentRoll) {
-  document.getElementById('current-roll2').innerText = currentRoll;
+  // document.getElementById('current-roll2').innerText = currentRoll;
   document.getElementById('round-score2').innerText = "You rolled 1, next player's turn!"
 } 
 
@@ -147,5 +182,5 @@ window.addEventListener("load", function() {
   document.getElementById('new-game').addEventListener('click', newGame);
   document.getElementById('button-roll').addEventListener('click', diceRoller);
   document.getElementById('button-pass').addEventListener('click', addToTotalScore);
-  document.getElementById('button-cheat').addEventListener('click', cheat)
+  // document.getElementById('button-cheat').addEventListener('click', cheat)
 });
