9d89eeb76b9b2055879e3694ec93781f09a1fea9
Alex Johnson <alex.johnson293@gmail.com>
2022-11-08 10:02:25 -0800

    pass .resetroundscore test
    Co-authored-by: Anton Ch <anton3ch@icloud.com>

--- __tests__/player.tests.js
+++ __tests__/player.tests.js
@@ -1,16 +1,25 @@
+// import test, { beforeEach } from 'node:test';
 import Player from './../src/player.js';
 
 describe('Player', () => {
+  const myPlayer = new Player();
 
-  test('should create an object Player with the appropriate properties', () => {
-    const myPlayer = new Player();
+  test('shall create an object Player with the appropriate properties', () => {
     expect(myPlayer.roundScore).toEqual(0);
     expect(myPlayer.totalScore).toEqual(0);
     expect(myPlayer.currentId).toEqual(0);
     expect(myPlayer.active).toEqual(1);
     expect(myPlayer.name).toEqual("");
+  });
 
+  test("shall increase the players round score by a given amount", () => {
+    myPlayer.playerAddRoundScore(5)
+    expect(myPlayer.roundScore).toEqual(5)
+  });
+
+  test("shall set the Player.roundScore to zero", () => {
+    myPlayer.roundScore = 5
+    myPlayer.playerResetRoundScore()
+    expect(myPlayer.roundScore).toEqual(0)
   });
 });
-
-// expect(myPlayer).toEqual(Player,{roundScore: 0, totalScore: 0, currentId: 0, active: 1, name: ""})
--- src/player.js
+++ src/player.js
@@ -1,7 +1,15 @@
-export default function Player(roundScore, totalScore, currentId, active, name) {
+export default function Player() {
     this.roundScore = 0;
     this.totalScore = 0;
     this.currentId = 0;
     this.active = 1;
     this.name = "";
+}
+
+Player.prototype.playerAddRoundScore = function(score) {
+  this.roundScore += score;
+}
+
+Player.prototype.playerResetRoundScore = function() {
+  this.roundScore = 0;
 }
